ad7fee3b4886052d05ec4ebf80c40f2d818da491
Hojiakbar <hojiakbarrasulovich@gmail.com>
2026-01-01 11:08:49 -0800

    Create next app and ShadCn

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+  
+function HomePage() {
+  return <div>
+    <Button>Hello</Button>
+  </div>;
+}
+
+export default HomePage;
